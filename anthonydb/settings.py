fc4361c0965de0a0b62b3096a706ebb50dfb4017
anthonydb <anthonymdebarros@gmail.com>
2023-06-19 17:22:30 -0400

    Minor template updates

--- anthonydb/settings.py
+++ anthonydb/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['127.0.0.1']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
+# ALLOWED_HOSTS = []
 
 # Application definition
 
