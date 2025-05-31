f7680084f3185f868945591a7cde0302c29ac247
anthonydb <anthonymdebarros@gmail.com>
2025-05-31 09:30:41 -0400

    Rework NYT Books API call per change to endpoint

--- data_miscellany/api.py
+++ data_miscellany/api.py
@@ -7,32 +7,29 @@ from .keys import *
 def fetch_nyt_booklist(list_type):
     # Construct API url
     api_key = nyt_api
-    base_url = 'https://api.nytimes.com/svc/books/v3/lists.json'
-    payload = {'api-key': api_key,
-               'list': list_type}
+    base_url = 'https://api.nytimes.com/svc/books/v3/lists/current/'
+    url = f"{base_url}{list_type}.json?api-key={api_key}"
 
     # Attempt to fetch the API and build a list of titles
     try:
-        r = requests.get(base_url, params=payload)
+        r = requests.get(url)
         nyt_list_response = r.json()
-        nyt_api_list_date_string = nyt_list_response['results'][0]['bestsellers_date']
+        nyt_api_list_date_string = nyt_list_response['results']['bestsellers_date']
         nyt_list_date = datetime.strptime(nyt_api_list_date_string, '%Y-%m-%d').strftime('%m/%d/%Y')
 
         # A list of dicts for titles
         top_list = []
 
-        for book in nyt_list_response['results']:
-            books_dict = {}
-            books_dict['rank'] = str(book['rank'])
-            if book['rank_last_week'] == 0:
-                books_dict['rank_lw'] = '-'
-            else:
-                books_dict['rank_lw'] = str(book['rank_last_week'])
-            books_dict['title'] = book['book_details'][0]['title']
-            books_dict['author'] = book['book_details'][0]['author']
-            books_dict['description'] = book['book_details'][0]['description']
-            books_dict['amazon_link'] = book['amazon_product_url']
-            books_dict['publisher'] = book['book_details'][0]['publisher']
+        for book in nyt_list_response['results']['books']:
+            books_dict = {
+                    'rank': str(book['rank']),
+                    'rank_lw': '-' if book['rank_last_week'] == 0 else str(book['rank_last_week']),
+                    'title': book['title'],
+                    'author': book['author'],
+                    'description': book['description'],
+                    'amazon_link': book['amazon_product_url'],
+                    'publisher': book['publisher']
+                }
             top_list.append(books_dict)
 
     except ValueError:
