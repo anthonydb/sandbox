46271b8d6eab57e14f8126fe5d9e95e8db170f95
anthonydb <anthonymdebarros@gmail.com>
2025-12-14 12:13:26 -0500

    Be dynamically accurate about the time in my current job lol

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from django.http import HttpResponsePermanentRedirect
+from datetime import date
 
 
 # def page_not_found_view(request, exception):
@@ -16,6 +17,15 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        start_date = date(2018, 9, 11)
+        today = date.today()
+        days_elapsed = (today - start_date).days
+        years_elapsed = round(days_elapsed / 365.25, 2)
+        context['years_elapsed'] = years_elapsed
+        return context
 
 
 class SandboxPageView(TemplateView):
